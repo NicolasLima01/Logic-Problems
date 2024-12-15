0b80b79150926257f7debe58c049364c271d80cc
Nicolas Lima <nicolasrdlima07@gmail.com>
2024-12-15 11:00:13 -0300

    Create the base function

---
+++ BubbleSort.js
@@ -0,0 +1,15 @@
+
+let numbers = [2, 1] //list of numbers
+
+function BubbleSort(...args) { //allows the function to treat an indefinite number of arguments as an array
+    let value1;
+    let value2;
+    // the loop will happen until the size of the array
+    for(i = 0; i < args.length; i++){
+        if(args[i] < args[i+1]){
+            
+            return args
+        }
+    }    
+}
+console.log(BubbleSort(numbers))
