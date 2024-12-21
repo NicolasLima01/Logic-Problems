fbadec1581873e4e775478646f1e396b1364e487
Nicolas Lima <nicolasrdlima07@gmail.com>
2024-12-21 20:54:18 -0300

    BubbleSort completed

--- BubbleSort.js
+++ BubbleSort.js
@@ -1,15 +1,16 @@
+let numbers = [3, 2, 1]; //args of numbers
 
-let numbers = [2, 1] //list of numbers
-
-function BubbleSort(...args) { //allows the function to treat an indefinite number of arguments as an array
-    let value1;
-    let value2;
-    // the loop will happen until the size of the array
-    for(i = 0; i < args.length; i++){
-        if(args[i] < args[i+1]){
-            
-            return args
+function BubbleSort(...args) { //allows the function to treat an indefinite number of arguments as an array 
+    for (let i = 0; i < args.length; i++) { //loop to iterate(repeat) through the array
+        for (let j = 0; j < args.length; j++) { //loop to compare the elements of the array
+            if (args[j + 1] < args[j]) {
+                let value = args[j + 1];
+                args[j + 1] = args[j];
+                args[j] = value;
+            }            
         }
-    }    
+    }
+    return args; //returns the sorted array
 }
-console.log(BubbleSort(numbers))
+console.log(BubbleSort(...numbers)); //prints the sorted list of numbers
+
