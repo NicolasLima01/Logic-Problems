7e2d65d526a4bf4dc279c69c0de43228a4c49f62
Nicolas Lima <nicolasrdlima07@gmail.com>
2024-12-22 11:39:39 -0300

    Creation of the function

--- Decimal_to_Binary.js
+++ Decimal_to_Binary.js
@@ -1,7 +1,15 @@
 //--------------------------------------------------------------------------------------------------
 //                                  Convert Decimal to Binary
 // 
-// This algorithm convert a decimal number into it's equivalent to binary number//
+// This algorithm convert a decimal number into it's equivalent to binary number
 //--------------------------------------------------------------------------------------------------
 
-let decimal = 4; //decimal number to be converted to binary
+Math.PI; // calling the library of math
+
+let number = 4; //decimal number to be converted to binary
+function decimal_to_binary(decimal){
+    let potency;
+    return potency;
+}
+
+console.log(decimal_to_binary(number));
