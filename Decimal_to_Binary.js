54417b67684085904bb887af7aacf0a0fc47efb8
Nicolas Lima <nicolasrdlima07@gmail.com>
2024-12-22 16:41:53 -0300

    Using the log function and switch case

--- Decimal_to_Binary.js
+++ Decimal_to_Binary.js
@@ -7,9 +7,14 @@
 Math.PI; // calling the library of math
 
 let number = 4; //decimal number to be converted to binary
-function decimal_to_binary(decimal){
-    let potency;
-    return potency;
+function decimal_to_binary(decimal) {
+    let potency = Math.log2(decimal); //Gets the potency of the number
+    let binary = "0";
+    switch (potency) {
+        case 0:
+            break;
+    }
+    return binary;
 }
 
-console.log(decimal_to_binary(number));
+console.log(decimal_to_binary(number)); //output must be 100
