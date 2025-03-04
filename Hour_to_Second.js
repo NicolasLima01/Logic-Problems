eeb85ca691eee06bbeece0934f07fee8b36546fe
NicolasLima01 <nicolasrlima@protonmail.com>
2025-03-04 19:20:51 -0300

    function convertHourToSecond working!

--- Hour_to_Second.js
+++ Hour_to_Second.js
@@ -3,8 +3,17 @@
 // 
 // This algorithm convert hour to second
 //--------------------------------------------------------------------------------------------------
-let hours = setHours(1, 10, 0, 0);
-let seconds;
-function convertHourToSecond(hours){
-    
+const timestamp = new Date("2022-10-10 11:05:00");
+let time = ("HH:MM:SS");
+
+function convertHourToSecond(time){
+    let hours = time.getHours();
+    let hours_seconds = hours * 3600;
+    let minutes = time.getMinutes();
+    let minutes_seconds = minutes * 60;
+    let seconds = time.getSeconds();
+    let totalSeconds = hours_seconds + minutes_seconds + seconds;
+    return totalSeconds;
 }
+
+console.log(convertHourToSecond(timestamp));
