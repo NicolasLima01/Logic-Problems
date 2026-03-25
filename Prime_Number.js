415006cd58a89ac24884844ecdfd7ade3ca2adf1
NicolasLima01 <nicolasrlima@protonmail.com>
2026-03-25 19:09:44 -0300

    Developing the prime number algorithm

--- Prime_Number.js
+++ Prime_Number.js
@@ -4,20 +4,25 @@
 
 */
 
-let i = 2
+let i = 2 //initiats in 2 to not count the number 1
 
-while (i <= 10) {
+while (i <= 100) {
 
     let prime = true;
 
+    // loop to verify if the number is prime
     for (let j = 2; j < i; j++) {
+
+        // if the number is divisible, it is not a prime number
         if (i % j === 0) {
             prime = false;
         }
+
     }
 
+    // verifies if the number is prime, prints the text
     if (prime == true) {
-        console.log(i + " é primo");
+        console.log(i + " is a prime number");
     }
 
     i = i + 1;
